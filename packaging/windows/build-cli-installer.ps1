d4561a1bb0708834a6283aba5ea1c9a6cfec736a
Atharva Sen Barai <89924185+AtharvaMoves@users.noreply.github.com>
2026-07-12 05:15:25 +0530

    Use standard NSIS ExecWait for CLI installer

--- packaging/windows/build-cli-installer.ps1
+++ packaging/windows/build-cli-installer.ps1
@@ -144,8 +144,7 @@ Section "Install"
   File /r "$payloadRootForNsis\*.*"
 
   DetailPrint "Installing shared Avikal core..."
-  nsExec::ExecToLog '"`$SYSDIR\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "`$INSTDIR\payload\packaging\windows\install-shared-core.ps1" -SourceRoot "`$INSTDIR\payload" -Version "$versionForNsis"'
-  Pop `$0
+  ExecWait '"`$SYSDIR\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "`$INSTDIR\payload\packaging\windows\install-shared-core.ps1" -SourceRoot "`$INSTDIR\payload" -Version "$versionForNsis"' `$0
   `${If} `$0 != "0"
     DetailPrint "Shared Avikal core installation failed."
     IfSilent +2 0
@@ -154,8 +153,7 @@ Section "Install"
   `${EndIf}
 
   DetailPrint "Installing Avikal CLI launcher..."
-  nsExec::ExecToLog '"`$SYSDIR\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "`$INSTDIR\payload\packaging\windows\install-cli-launcher.ps1" -Version "$versionForNsis"'
-  Pop `$0
+  ExecWait '"`$SYSDIR\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "`$INSTDIR\payload\packaging\windows\install-cli-launcher.ps1" -Version "$versionForNsis"' `$0
   `${If} `$0 != "0"
     DetailPrint "Avikal CLI launcher installation failed."
     IfSilent +2 0
